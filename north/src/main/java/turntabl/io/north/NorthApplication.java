85b05103c0b12dc6988a51d5d689d738a33cdabb
patricia-agyekum <patriciakasapa07@gmail.com>
2019-11-20 11:19:00 +0000

    Starting

---
+++ north/src/main/java/turntabl/io/north/NorthApplication.java
@@ -0,0 +1,13 @@
+package turntabl.io.north;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthApplication.class, args);
+	}
+
+}
---
+++ north/src/test/java/turntabl/io/north/NorthApplicationTests.java
@@ -0,0 +1,13 @@
+package turntabl.io.north;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
